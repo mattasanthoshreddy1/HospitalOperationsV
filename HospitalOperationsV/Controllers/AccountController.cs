dc3ef30bc6e069a6519ae368e369d4f92321e88b
v-matred <v-matred@MININT-6JOJ1ON>
2018-09-21 20:33:50 +0530

    Add project files.

---
+++ HospitalOperationsV/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using EntitiesOperation;
+using HospitalOperationsV.Models;
+using HospitalOperationsV.Security;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace HospitalOperationsV.Controllers
+{
+    public class AccountController : Controller
+    {
+        OperationsEntities Context = new OperationsEntities();
+        // GET: Account
+        [HttpGet]
+        public ActionResult Index() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model, string returnUrl = "")
+        {
+            if (ModelState.IsValid)
+            {
+                var user = Context.LoginDetails.Where(u => u.Username == model.Username && u.Password == model.Password).FirstOrDefault();
+                if (user != null)
+                {
+                    var roles = Context.Roles.Select(m => m.RoleName).ToArray();
+
+                    CustomPrincipalSerializeModel serializeModel = new CustomPrincipalSerializeModel();
+                    serializeModel.UserId = user.UserId;
+                    serializeModel.FirstName = user.FirstName;
+                    serializeModel.LastName = user.LastName;
+                    serializeModel.roles = roles;
+
+                    string userData = JsonConvert.SerializeObject(serializeModel);
+                    FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
+                             1,
+                            user.Email,
+                             DateTime.Now,
+                             DateTime.Now.AddMinutes(15),
+                             false,
+                             userData);
+
+                    string encTicket = FormsAuthentication.Encrypt(authTicket);
+                    HttpCookie faCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encTicket);
+                    if(model.RememberMe == true)
+                      Response.Cookies.Add(faCookie);
+
+                    if (roles.Contains("Admin"))
+                    {
+                        return RedirectToAction("Index", "Admin");
+                    }
+                    else if (roles.Contains("User"))
+                    {
+                        return RedirectToAction("Index", "User");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+                ModelState.AddModelError("", "Incorrect username and/or password");
+            }
+
+            return View(model);
+        }
+    }
+}
